fef8f8304ffd9b344c1f4344c96bfdfaa10a1ad2
Ahmed Ragab <120670728+A7med7c@users.noreply.github.com>
2026-01-11 14:31:47 +0200

    Cursor & DB Administration

---
+++ Day10/SQLQuery1.sql
@@ -0,0 +1,22 @@
+--1.	Create a cursor for Employee table that increases Employee salary by 10% if Salary <3000
+--and increases it by 20% if Salary >=3000. Use company DB
+
+
+
+
+
+
+
+
+
+
+
+
+
+2.	Display Department name with its manager name using cursor. Use ITI DB
+3.	Try to display all students first name in one cell separated by comma. Using Cursor 
+4.	Create a sequence object that allow values from 1 to 10 without cycling in a specific column and test it.
+5.	Create snapshot of adventureworks_db and test it
+6.	Transform all functions in day2 to Stored Procedures
+7.	Create full, differential Backup for SD DB.
+8.	Use display student’s data (ITI DB) in excel sheet
